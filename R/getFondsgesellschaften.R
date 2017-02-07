6eab81f0306bea2848ccde0aed0e29da00bc5d1c
bowerth <bo.werth@gmail.com>
2017-02-07 23:33:47 +0100

    add function

---
+++ R/getFondsgesellschaften.R
@@ -0,0 +1,17 @@
+#' getFG
+#'
+#' Get Fondsgesellschaften
+#'
+#' Function to return list of Fondsgesellschaften
+#'
+#' @param mode charachter initial letter of label
+#' @importFrom magrittr %>%
+#' @export
+
+getFG <- function() {
+  return()
+}
+
+urlFG <- function() {
+  return()
+}
